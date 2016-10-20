6db81bb3f3aa8242dfd0e00782d05f5f385ba195
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2016-10-20 15:28:08 -0700

    Not working input. Won't listen to the changes in the input

--- addon/components/temp-converter.js
+++ addon/components/temp-converter.js
@@ -2,20 +2,35 @@ import Ember from 'ember';
 console.log("Ember converter.js is here");
 
 export default Ember.Component.extend({
-  foo: 0,
   typesOfTemp: ['Celsius', 'Fahrenheit'],
   tempType: 'Celsius',
+  userTemp: 0,
+  data: null,
 
   actions: {
     selectTempType(tempType) {
       this.set('tempType', tempType);
     },
-    // setInputTempVal(temp) {
-    //   console.log('Inside the setInputTempVal');
-    //   this.set('temp', temp);
-    // },
-    convertTemp: function(){
+
+    changed(data) {
+      console.log("value is changed");
       debugger;
+      console.log(data);
+    },
+
+    // setInputTempVal(userTemp) {
+    //   debugger;
+    //   console.log('Inside the setInputTempVal');
+    //   this.set('userTemp', userTemp);
+    // },
+
+    convertTemp: function(){
+      var t;
+      console.log('Inside the convertTemp' + t);
+
+      t = this.get('userTemp');
+  
+      console.log('And now' + t);
     }
   }
 });
